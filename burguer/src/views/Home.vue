44964b13cacf826925df53ba2ea6d8e123a3a36d
juninhoww2 <alqadianoKira@gmail.com>
2021-10-28 16:30:53 -0300

    Home

--- burguer/src/views/Home.vue
+++ burguer/src/views/Home.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="main-container">
-    <h1>HOME</h1>
+  <div>
+    <Banner />
+    <div class="main-container">
+        <h1>Mostre o seu burger:</h1>
+        <BurgerForm />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import Banner from '../components/Banner'
+import BurgerForm from '../components/BurgerForm.vue'
 
 export default {
   name: 'Home',
   components: {
-
-  }
+      Banner,
+      BurgerForm
+    }
 }
 </script>
